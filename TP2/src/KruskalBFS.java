fcb16c03fb0594fc91681a34db5b650cc72a1e74
reiva <reiva@192.168.2.102>
2021-10-08 11:22:29 -0300

    test grafos

--- TP2/src/KruskalBFS.java
+++ TP2/src/KruskalBFS.java
@@ -5,21 +5,7 @@ public class KruskalBFS {
 	
 	
 	public static void main(String[] args) {
-		Grafo g = new Grafo(5);
-		g.agregarArista(1, 0);
-		g.agregarArista(0, 4);
-		g.agregarArista(2, 3);
-		
-		Grafo g2 = new Grafo(7);
-		g2.agregarArista(0, 1);
-		g2.agregarArista(0, 2);
-		g2.agregarArista(1, 3);
-		g2.agregarArista(2, 4);
-		g2.agregarArista(1, 4);
-		
-		g2.agregarArista(5, 6);
-		
-		Util.esConexo( g);
+	
 	}
 	
 	
